bfad50da957588b0fed0c057a7e9b3824677fa13
Kenny Xu (Laptop) <xxkennyxu@gmail.com>
2014-12-31 03:24:32 -0800

    styled post-containers with mock data

--- app/controllers.js
+++ app/controllers.js
@@ -1,14 +1,22 @@
 var starBoxController = angular.module('starBoxController', []);
 
+function randomDate(start, end) {
+    return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+}
+
 starBoxController.controller('MainPageCtrl', ['$scope', '$http',
 	function ($scope, $http) {
 		var data = [];
 		for (var i = 0, l= 20; i<l; i++) {
+			var images = ["../mock-assets/janna50.png", "../mock-assets/chogath50.png", "../mock-assets/gilbert50.png"]
+			var authors = ["Janna", "Cho'Gath", "Gilbert"];
+			var titles = ["Blizzard Rained Upon Us","I Swallow Your Death", "NOMNOMNOM"];
 			data.push({
-				title: "Title " + i,
-				author: "Author " + i,
-				date: "Date " + i,
-				body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo ultricies commodo. Aliquam ut ipsum ante. Suspendisse commodo tempor lacus, id fringilla metus ultricies non. Donec sodales augue magna, eget viverra libero aliquet id. Mauris ut elementum mauris, nec luctus nibh. Mauris finibus rhoncus urna, vel ullamcorper neque tincidunt eget. Pellentesque et elit ex. Nunc at elementum enim. Pellentesque mattis euismod felis et elementum. Ut vestibulum egestas mi, a consectetur tellus mattis a. Aenean ante lorem, finibus sit amet molestie eget, varius ac odio. Maecenas nunc lorem, suscipit sed pretium vitae, ornare a leo. Nunc hendrerit ultricies libero in eleifend. " + i
+				image: images[i%3],
+				author: authors[i%3],
+				title: titles[i%3],
+				date: randomDate(new Date(2012, 0, 1), new Date()).toUTCString(),
+				body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus commodo ultricies commodo. Aliquam ut ipsum ante. Suspendisse commodo tempor lacus, id fringilla metus ultricies non. Donec sodales augue magna, eget viverra libero aliquet id. Mauris ut elementum mauris, nec luctus nibh. Mauris finibus rhoncus urna, vel ullamcorper neque tincidunt eget. Pellentesque et elit ex. Nunc at elementum enim. Pellentesque mattis euismod felis et elementum. Ut vestibulum egestas mi, a consectetur tellus mattis a. Aenean ante lorem, finibus sit amet molestie eget, varius ac odio. Maecenas nunc lorem, suscipit sed pretium vitae, ornare a leo. Nunc hendrerit ultricies libero in eleifend. "
 			});
 		}
 		$scope.data = data;
