a7d87dfc2261a8dc40682d3b09f9ef8419ddebdb
Kenny Xu (Surface) <xxkennyxu@gmail.com>
2015-01-02 23:47:23 -0800

    slick image viewer populated with fixture data

--- app/controllers.js
+++ app/controllers.js
@@ -4,8 +4,38 @@
 (function () {
     // Throws exceptions on un-safe practices
     "use strict";
-    var starBoxController = angular.module('starBoxController', []);
-
+    var starBoxController = angular.module('starBoxController', [])
+        .directive("imageCarouselDirective", function() {
+            return function(scope, element, attributes) {
+                if(scope.$last) {
+                    scope.$emit('lastimageloaded');   
+                }
+            };
+        })
+        .directive("slickifyDirective", function() {
+            return function(scope, element, attributes) {
+                scope.$on('lastimageloaded', function (event) {
+                    // Slickify here
+                     $('.main-image-show').slick({
+                        slidesToShow: 1,
+                        slidesToScroll: 1,
+                        arrows: false,
+                        fade: true,
+                        asNavFor: '.main-image-nav'
+                    });
+                    $('.main-image-nav').slick({
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        asNavFor: '.main-image-show',
+                        dots: true,
+                        centerMode: true,
+                        focusOnSelect: true
+                    });
+                });
+            };
+        });   
+        
+        
     function randomDate(start, end) {
         return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     }
@@ -13,10 +43,10 @@
     starBoxController.controller('MainPageCtrl', ['$scope', '$http',
         function ($scope, $http) {
             var data = [], i = 0, l = 20;
-            for (i; i < l; i++) {
-                var images = ["../mock-assets/janna50.png", "../mock-assets/chogath50.png", "../mock-assets/gilbert50.png"];
+                var images = ["../picture%20assets/janna50.png", "../picture%20assets/chogath50.png", "../picture%20assets/gilbert50.png"];
                 var authors = ["Janna", "Cho'Gath", "Gilbert"];
                 var titles = ["Blizzard Rained Upon Us", "I Swallow Your Death", "NOMNOMNOM"];
+            for (i; i < l; i++) {
                 data.push({
                     image: images[i % 3],
                     author: authors[i % 3],
@@ -27,12 +57,18 @@
             }
             $scope.data = data;
             
-            // Slickify here
-            $scope.$on('$viewContentLoaded', function (){
-                console.log("Hello");
-                $('.main-image-carousel').slick({
-                });
-            });
+            var images = [], i = 0, l = 4;
+            var headers = ["Atlantica Released!!!", "PATCH - Chase Them To The Skies!", "The Apocalypse!", "Spaceships Are Attacking Us!"];
+            for(i; i < l; i++) {
+                images.push({
+                    image: "../picture%20assets/image" + (i+1) + ".png",
+                    header: headers[i],
+                    description: "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas" 
+                });   
+            }
+            
+            $scope.images = images;
+            
         }]);
 
     starBoxController.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
